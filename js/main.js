c83d717810714f4a3277497bd07530bfb1c7cf16
rxphaelbihag <raphaelbihag00@gmail.com>
2024-06-04 20:20:57 +0800

    dynamic changes for section & profile bg

--- js/main.js
+++ js/main.js
@@ -40,12 +40,16 @@ async function fetchData(filePath) {
 
 function load_cards(section) {
     var file_path;
+    let bg = document.querySelector(".bg"); 
     if (section == 'curie') {
         file_path = '../data/curie_sample.json';
+        bg.value = 'CURIE';
     } else if (section == 'tesla') {
         file_path = '../data/tesla_sample.json';
+        bg.value = 'TESLA';
     } else if (section == 'einstein') {
         file_path = '../data/einstein_sample.json';
+        bg.value = 'EINSTEIN';
     };
     fetchData(file_path)
     .then(data => {
